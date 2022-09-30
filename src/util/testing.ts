9ff22588a476de3532b54d790442e75608ead01f
Thomas Price <thomaswprice1@gmail.com>
2022-09-29 22:07:36 -0400

    "Throw it in!"

---
+++ src/util/testing.ts
@@ -0,0 +1,11 @@
+import * as Matter from 'matter-js';
+// let bodies = 
+// let collisionDetector = Matter.Detector.create()
+
+// let bodies = objects
+// Matter.Detector.setBodies(collisionDetector, bodies)
+
+// Matter.Detector.collisions(collisionDetector)
+
+// // each physics object could get the list of objects from the world, filter it to physics objects, then get each body from each physics object, and then use query on that list of bodies
+// Matter.Query.collides()
