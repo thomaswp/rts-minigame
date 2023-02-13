448394d7b3710e36a44bb79e8836affdaee64c2f
Thomas Price <thomaswprice1@gmail.com>
2023-02-12 20:37:46 -0500

    Updated npm and packages; added particle emitter

---
+++ src/objects/effects/Explosion.ts
@@ -0,0 +1,17 @@
+// import { Container, DisplayObject } from "pixi.js";
+// import { WorldObject } from "../WorldObject";
+
+// export class Explosion extends WorldObject {
+
+//     // particleContainenpm install @pixi/particle-emitterr: Container;
+//     emitter: PIXI.particles.Emitter;
+
+//     constructor() {
+//         super();
+//         // this.particleContainer = new Container();
+//     }
+
+//     getDisplayObject(): DisplayObject {
+//         // return this.particleContainer;
+//     }
+// }
